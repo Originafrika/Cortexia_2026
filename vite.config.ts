c02032b6f2adefebf0b8385bab3a0c279acdca56
Kheir Lissi <juniorkheir@gmail.com>
2026-04-20 16:55:29 -0400

    fix: vite-plugin-vercel import

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from 'vite-plugin-vercel';
+import vercelAdapter from 'vite-plugin-vercel';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), vercel()],
+  plugins: [react(), tailwindcss(), vercelAdapter],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
